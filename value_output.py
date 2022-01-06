2761a05163fd29afdefeb0de0b32078e3ed39b5f
k-the-p <97202820+k-the-p@users.noreply.github.com>
2022-01-06 09:41:54 +0900

    first commit

---
+++ value_output.py
@@ -0,0 +1,57 @@
+#import os
+#from contextlib import redirect_stdout
+import sys
+import cshogi
+import cshogi.KIF
+from cshogi.usi import Engine
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini", encoding="utf-8")
+board = cshogi.Board() #使わないけど何かの際に
+
+def listoutput():
+    #エンジン読み込み、設定変更はここ
+    engine = Engine(config.get("DEFAULT", "Enginepath") , debug=False)
+    engine.setoption("NodesLimit", config.get("DEFAULT", "Nodeslimit"))
+
+    #以下は触らない
+    engine.setoption("MultiPV", "2")
+    engine.isready()
+
+    #棋譜読み込み
+    path = 'kifu.kif'
+    with open(path, encoding="UTF-8", mode="r") as f:
+        kif = cshogi.KIF.Parser.parse_str(f.read())[0]
+    kiflist = kif['moves']
+    #kiflist = ['7g7f', '3c3d', '7i6h', '2b6f'] #テスト用
+
+    #out.txt初期化
+    with open("out.txt", 'w') as f:
+        f.truncate(0)
+
+
+    #棋譜に従って手を進める
+    j = []
+    for i in kiflist:
+        j.append(i)
+        engine.position(j)
+        # #ここから下はなんとかしたかったけど不明
+        sys.stdout = open('out.txt', 'a')
+        engine.go(listener=print)
+        # k.append(output)
+        sys.stdout = sys.__stdout__ # 元に戻す
+    return kif['moves']
+
+def sente():
+    path = 'kifu.kif'
+    with open(path, encoding="UTF-8", mode="r") as f:
+        kif = cshogi.KIF.Parser.parse_str(f.read())[0] 
+    return("先手 " + str(kif['names'][cshogi.BLACK]).replace("None", ""))
+    #return kif['moves']
+def gote():
+    path = 'kifu.kif'
+    with open(path, encoding="UTF-8", mode="r") as f:
+        kif = cshogi.KIF.Parser.parse_str(f.read())[0] 
+
+    return("後手 " + str(kif['names'][cshogi.WHITE]).replace("None", ""))
---
+++ wrapper.py
@@ -0,0 +1,7 @@
+import value_output
+import graph
+
+def result():
+    call = value_output.listoutput() #out.txtへの書き込みを行ってから
+    responce = graph.imageoutput() #グラフを作画
+    return (responce)
